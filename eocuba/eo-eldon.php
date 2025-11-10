bc207483f668a23386975f7739dd67a223e8f366
esperantocuba <kuba.esperanto.asocio@gmail.com>
2025-11-09 23:06:55 -0800

    Add files via upload

---
+++ eocuba/eo-eldon.php
@@ -0,0 +1,47 @@
+<?php
+   include ( 'er/kap01' ) ;
+?>
+
+    <title>Esperanto Cuba . org</title>
+	<meta name="description" content="Retpaĝaro de Esperanto Cuba.">
+	<meta name="keywords" content="Esperanto, Cuba">
+
+	<meta name="author" content="trio@esperanto.org">
+
+<?php
+   include ( 'er/kap02' ) ;
+?>
+
+</head>
+<body>
+
+<?php
+   include ( 'er/hamburgero' ) ;
+?>
+
+
+<!-- Ĉefa kapa enhavo -->
+<header class="main-header">
+    <h1><a href="index2.php">Esperanto Cuba . org</a></h1>
+    <p>-- paĝ-nomo --</p>
+</header>
+
+<?php
+   include ( 'er/hamburger-nav' ) ;
+?>
+
+<h2>-- paĝ-nomo --</h2>
+<p style="text-align: center;">Bonvole atendu ĝis ni re-kreas retpaĝaron.</p>
+
+<p><strong>-- paĝ-nomo --</strong></p>
+<p>&nbsp;</p>
+<p>-- paĝ-nomo ---- paĝ-nomo ---- paĝ-nomo ---- paĝ-nomo ---- paĝ-nomo ---- paĝ-nomo ---- paĝ-nomo ---- paĝ-nomo --</p>
+<p>&nbsp;</p>
+<p>-- paĝ-nomo --</p>
+<p>&nbsp;</p>
+
+<hr> <!-- Ĉi tiu devas esti farita pere de alia stilo... malgraŭ ĉi tiu estas multe pli facila. :) -->
+
+<?php
+   include ( 'er/pied' ) ;
+?>
---
+++ eocuba/index2.php
@@ -0,0 +1,47 @@
+<?php
+   include ( 'er/kap01' ) ;
+?>
+
+    <title>Esperanto Cuba . org</title>
+	<meta name="description" content="Retpaĝaro de Esperanto Cuba.">
+	<meta name="keywords" content="Esperanto, Cuba">
+
+	<meta name="author" content="trio@esperanto.org">
+
+<?php
+   include ( 'er/kap02' ) ;
+?>
+
+</head>
+<body>
+
+<?php
+   include ( 'er/hamburgero' ) ;
+?>
+
+
+<!-- Kapo de la korpa enhavo -->
+<header class="main-header">
+    <h1>Esperanto Cuba . org</h1>
+    <p>El esperanto es un portal a otras culturas.</p>
+</header>
+
+<?php
+   include ( 'er/hamburger-nav' ) ;
+?>
+
+<h2>Ĉefpaĝo</h2>
+<p style="text-align: center;">Bonvole atendu ĝis ni re-kreas retpaĝaron.</p>
+
+<p><strong>Esperanto: la lengua internacional</strong></p>
+<p>&nbsp;</p>
+<p>Las personas usan el esperanto para una gran variedad de objetivos, pero en especial para hacer amigos con personas de diversos países.</p>
+<p>&nbsp;</p>
+<p>El objetivo no es reemplazar las otras lenguas sino ser un punto de encuentro entre las diferentes comunidades lingüísticas.</p>
+<p>&nbsp;</p>
+
+<hr> <!-- Ĉi tiu devas esti farita pere de alia stilo... malgraŭ ĉi tiu estas multe pli facila. :) -->
+
+<?php
+   include ( 'er/pied' ) ;
+?>
